e9b500a6e4ed6a4ff306b057bba36b24108441e0
petarpetrov <ipetrovbg@gmail.com>
2021-03-21 16:10:43 +0200

    add rust

---
+++ rust/src/lib.rs
@@ -0,0 +1,12 @@
+use wasm_bindgen::prelude::*;
+
+#[wasm_bindgen]
+pub fn add(a: i32, b: i32) -> i32 {
+    a + b
+}
+
+#[wasm_bindgen]
+pub fn hello() -> String {
+    String::from("Hello World!")
+}
+
--- src/Main.elm
+++
@@ -1,8 +0,0 @@
-module Main exposing (main)
-
-import Html exposing (h1, text)
-import Html.Attributes exposing (class)
-
-
-main =
-    h1 [ class "text-xl" ] [ text "Hello Wold!" ]
